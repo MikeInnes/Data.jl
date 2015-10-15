48776ba3a3ef2ec9b686880499346e33bcd49151
Mike Innes <mike.j.innes@gmail.com>
2015-10-15 11:08:29 +0100

    nullable vartypes

--- src/nulls.jl
+++ src/nulls.jl
@@ -4,3 +4,5 @@ export NullableArray, NullableVector
 
 typealias NullableArray{T, N} AbstractArray{Nullable{T}, N}
 typealias NullableVector{T} NullableArray{T, 1}
+
+vartype{T}(::Type{Nullable{T}}) = vartype(T)
