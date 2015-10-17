00769d7f19081eb58992af7f6e3208bb88b210c8
Mike Innes <mike.j.innes@gmail.com>
2015-10-17 17:35:36 +0100

    support column slices

--- src/tables/views.jl
+++ src/tables/views.jl
@@ -1,21 +1,26 @@
-import Base: getindex, length
+import Base: getindex, length, names
 import ArrayViews: view
 
 immutable DataView{T, I} <: Table
   data::T
+  cols::Vector{Symbol}
   index::I
 end
 
 typealias DataRow{T, I<:Integer} DataView{T, I}
 
-view(data::Table, i) = DataView(data, i)
+view(data::Table, cols, i) = DataView(data, cols, i)
+view(data::Table, i) = view(data, Symbol[], i)
 
-view(d::DataView, i) = DataView(d.data, d.index[i])
+view(d::DataView, cols, i) = DataView(d.data, cols, d.index[i])
+view(d::DataView, i) = view(d.data, Symbol[], d.index[i])
 
 getindex(d::DataView, f::Column) = sub(d.data[f], d.index)
 getindex(d::DataView, f::Column, i) = d.data[f, d.index[i]]
 getindex(d::DataView, i) = d.data[d.index[i]]
 
 length{T, I<:AbstractArray}(d::DataView{T, I}) = length(d.index)
+
+names(d::DataView) = isempty(d.cols) ? names(d.data) : d.cols
 
 @forward DataView.data Base.names
